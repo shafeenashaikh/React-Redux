ec0123af890702c1b6c29f79ecceab1c5711b1b3
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-09-21 11:25:50 +0530

    adding items to cart in redux

--- src/Redux-Compoment/Home.js
+++ src/Redux-Compoment/Home.js
@@ -1,10 +1,30 @@
-import React from "react";
-
+import React from "react"
 function Home(){
+    
     return(
         <div>
+            <div className="add-to-cart">
+                <img src="/C:\Users\intel\Desktop\redux-tut\src\img\cart.jpg"/>
+            </div>
             <h2>Home component</h2>
+            <div className="cart-wrapper">
+                <div className="imag-wrapper item">
+                   <img src='/public/img/image1.jpg'/>
+                </div>
+                <div className="text-wrapper item">
+                  <span>
+                      I-Phone
+                  </span>
+                  <span>
+                    price: $10000
+                  </span>
+                </div>
+                <div className="btn-wrapper item">
+                    <button>Add to Cart</button>
+                </div>
+            </div>
         </div>
+
     )
 }
 export default Home
---
+++ src/imagecomponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ImageComponent extends Component {
+    render() {
+        return (
+            <div>
+                <img src="/public/img/image1.jpg" alt="display image" />
+            </div>
+        );
+    }
+}
+
+export default ImageComponent;
