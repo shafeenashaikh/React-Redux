f8eb54f4f0ca5648b04a557b000f429d18edf5e7
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-09-21 18:46:27 +0530

    adding data to HeaderContainer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Parent from './Components/Parent';
 import HomeContainer from './Container/HomeContainer'
+import HeaderContainer from './Container/HeaderContainer';
 function App() {
   return (
     <div className="App">
       {/* <Parent/> */}
+      <HeaderContainer/>
     <HomeContainer/>
     </div>
   );
