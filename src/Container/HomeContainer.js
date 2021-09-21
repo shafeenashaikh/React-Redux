50aad68fc183ecb9153b4328289b1489ccffa59e
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-09-21 12:55:41 +0530

    created container to connect react with redux

--- src/Container/HomeContainer.js
+++ src/Container/HomeContainer.js
@@ -1,3 +1,15 @@
+import {connect} from 'react-redux';
 import Home from  '../Redux-Compoment/Home';
+import { addToCart } from '../Services/Action/action';
 
-export default Home
+const mapStateToProps=state => ({
+
+})
+
+const mapDispatchToProps=dispatch=>({
+    addToCartHandler:data=>dispatch(addToCart(data))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
+
+// export default Home
--- src/Services/Action/action.js
+++ src/Services/Action/action.js
@@ -1,6 +1,6 @@
 import ADD_TO_CART from '../constant'
 
-export const addToCart = (data)=> {
+export const addToCart=(data)=> {
     return {
     type: ADD_TO_CART,
     data: data
