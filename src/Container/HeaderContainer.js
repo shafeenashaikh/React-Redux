a57a5bd8162adc49bc96734c651338b926e6d534
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-09-21 14:42:22 +0530

    created Header and HeaderContainer

---
+++ src/Container/HeaderContainer.js
@@ -0,0 +1,15 @@
+import {connect} from 'react-redux';
+import Header from  '../Redux-Compoment/Header';
+import { addToCart } from '../Services/Action/action';
+
+const mapStateToProps=state => ({
+
+})
+
+const mapDispatchToProps=dispatch=>({
+   
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
+
+// export default Home
